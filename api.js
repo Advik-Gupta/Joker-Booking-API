7adedddf2ca28ed48b5aef38c4a4f95398385fc8
Advik Gupta <fortknight6901@gmail.com>
2022-05-25 17:46:10 +0530

    finished app

--- api.js
+++ api.js
@@ -1,23 +1,23 @@
-const axios = require('axios');
-const express = require('express');
-var TourCMSApi = require('tourcms');
+const axios = require("axios");
+const express = require("express");
+var TourCMSApi = require("tourcms");
 const app = express();
 const port = process.env.PORT || 3030;
 const url = `https://wave3app-staging.herokuapp.com/mark_api/v1`;
-const cors = require('cors');
-const date = require('date-and-time');
+const cors = require("cors");
+const date = require("date-and-time");
 // ___________________________________________________________________________________________________________________________
 
-const firebase = require('firebase');
+const firebase = require("firebase");
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyDe6xx7mZOoMtkjbO62Cb7o9KEB7dbMhv8',
-	authDomain: 'joker-booking.firebaseapp.com',
-	projectId: 'joker-booking',
-	storageBucket: 'joker-booking.appspot.com',
-	messagingSenderId: '918282203466',
-	appId: '1:918282203466:web:72b24b56b04c3db28491b3',
-	measurementId: 'G-E7NEEVW7XM'
+  apiKey: "AIzaSyDe6xx7mZOoMtkjbO62Cb7o9KEB7dbMhv8",
+  authDomain: "joker-booking.firebaseapp.com",
+  projectId: "joker-booking",
+  storageBucket: "joker-booking.appspot.com",
+  messagingSenderId: "918282203466",
+  appId: "1:918282203466:web:72b24b56b04c3db28491b3",
+  measurementId: "G-E7NEEVW7XM",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -30,51 +30,98 @@ app.use(cors());
 
 // ___________________________________________________________________________________________________________________________
 
-app.get('/trial', async (req, res) => {
-	res.send('Api working');
+app.get("/trial", async (req, res) => {
+  res.send("Api working");
 });
 
-app.get('/', async (req, res) => {
-	const data = [];
-	await db.collection('data').get().then((snapshot) => {
-		snapshot.docs.map((doc) => {
-			let { bronze, dates, silver, gold } = doc.data();
-			let documentData = {
-				bronze,
-				dates,
-				silver,
-				gold
-			};
-			data.push(documentData);
-		});
-	});
-	res.send(data);
+app.post("/tailoryourjourney", async (req, res) => {
+  console.log(req.body);
+  let responseData = {};
+
+  let config = {
+    method: "post",
+    url: "https://vv.wildfire.buzz/wp-json/gf/v2/entries/",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic Y2tfOGJkMDhiNTg0MzNkZDRkMjc0ZTVmMGFlNTc1M2U4Y2Y4OTdhNWY1OTpjc18wM2E2NDk1MWM2NzcxM2RhNWUwOWJiNmMxY2FiMjNkOTFlY2ViNmJk",
+    },
+    data: req.body,
+  };
+
+  axios(config)
+    .then(function (response) {
+      responseData = JSON.stringify(response.data);
+      let ID = JSON.parse(responseData).id;
+
+      let newConfig = {
+        method: "post",
+        url: `https://vv.wildfire.buzz/wp-json/gf/v2/entries/${ID}/notifications`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Basic Y2tfOGJkMDhiNTg0MzNkZDRkMjc0ZTVmMGFlNTc1M2U4Y2Y4OTdhNWY1OTpjc18wM2E2NDk1MWM2NzcxM2RhNWUwOWJiNmMxY2FiMjNkOTFlY2ViNmJk",
+        },
+      };
+
+      axios(newConfig)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+          res.send(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
-app.get('/:id', async (req, res) => {
-	const { id } = req.params;
-	await db
-		.collection('data')
-		.doc(id)
-		.get()
-		.then((snapshot) => {
-			let { bronze, dates, silver, gold } = snapshot.data();
-			let documentData = {
-				bronze,
-				dates,
-				silver,
-				gold
-			};
-			res.send(documentData);
-		})
-		.catch((error) => {
-			res.send(error);
-			console.log(error);
-		});
+app.get("/", async (req, res) => {
+  const data = [];
+  await db
+    .collection("data")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.map((doc) => {
+        let { bronze, dates, silver, gold } = doc.data();
+        let documentData = {
+          bronze,
+          dates,
+          silver,
+          gold,
+        };
+        data.push(documentData);
+      });
+    });
+  res.send(data);
+});
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  await db
+    .collection("data")
+    .doc(id)
+    .get()
+    .then((snapshot) => {
+      let { bronze, dates, silver, gold } = snapshot.data();
+      let documentData = {
+        bronze,
+        dates,
+        silver,
+        gold,
+      };
+      res.send(documentData);
+    })
+    .catch((error) => {
+      res.send(error);
+      console.log(error);
+    });
 });
 
 // ___________________________________________________________________________________________________________________________
 
 app.listen(port, () => {
-	console.log(`Example app listening at ${port}`);
+  console.log(`Example app listening at ${port}`);
 });
